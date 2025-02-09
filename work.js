055ab34df5c7dc8654a4806556bf324bfbd7d307
Manny Ledoux <manny@aledoux.net>
2025-02-09 14:24:19 -0500

    fixes merge conflicts and adds a fibonnaci sequence

--- work.js
+++ work.js
@@ -5,3 +5,12 @@ const subtract = (a, b) => a - b;
 const multiply = (a,b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const a = (c,d,e) => c + d + e;
+
+const fibonnacci = (n) => {
+  if (n <= 1) {
+      return n;
+  }
+  return fibonnacci(n - 1) + fibonnacci(n - 2);
+}
